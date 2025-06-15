f7d2defc69104577a24d0fd38343cb5d2b5c07f1
danielhaimov <danielhaimov8@gmail.com>
2025-06-15 22:56:09 +0300

    page.tsx

--- apps/web/src/app/[lang]/movies/[id]/page.tsx
+++ apps/web/src/app/[lang]/movies/[id]/page.tsx
@@ -1,35 +1,11 @@
-import type { Metadata } from 'next'
-
-import { MovieDetails } from './_components/movie-details'
-
-import { getMovieMetadata } from '@/utils/seo/get-movie-metadata'
-import { getMoviesIds } from '@/utils/seo/get-movies-ids'
-
-import type { PageProps } from '@/types/languages'
-import { Suspense } from 'react'
-
-type MoviePageProps = PageProps<{ id: string }>
-
 export async function generateStaticParams() {
-  const moviesIds = await getMoviesIds()
-  return moviesIds.map(id => ({ id: String(id) }))
+  try {
+    const moviesIds = await getMoviesIds()
+    return moviesIds.map(id => ({ id: String(id) }))
+  } catch (error) {
+    console.error("⚠️ Failed to generate static params:", error)
+    return [] // מונע קריסה
+  }
 }
 
-export async function generateMetadata(
-  props: MoviePageProps
-): Promise<Metadata> {
-  const { lang, id } = await props.params
-  const metadata = await getMovieMetadata(Number(id), lang)
-
-  return metadata
-}
-
-export default async function MoviePage(props: MoviePageProps) {
-  const { id, lang } = await props.params
-
-  return (
-    <Suspense>
-      <MovieDetails id={Number(id)} language={lang} />
-    </Suspense>
-  )
-}
+export const dynamic = 'force-dynamic'; // אם אתה לא חייב SSG
